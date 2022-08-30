9d6229bf3db2fa97bdfe88111df4ad6799a419fd
Emiliano <emiliano.hotes@despegar.com>
2022-08-29 21:27:04 -0300

    feat: created MyProfile intro file

---
+++ 01-variables/MyProfile.java
@@ -0,0 +1,9 @@
+public class MyProfile {
+    public static void main(String[] args) {
+        String name = "Carlos";
+        int age = 23;
+        double desiredSalary = 1000.00;
+        char gender = 'm';
+        boolean lookingForJob = false;
+    }
+}
