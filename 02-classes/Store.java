82c77a2626e4c9004acc0eee1673c942d5b3743d
Emiliano <emiliano.hotes@despegar.com>
2022-08-29 21:37:29 -0300

    feat: called method advertise 3 times

--- 02-classes/Store.java
+++ 02-classes/Store.java
@@ -2,20 +2,22 @@ public class Store {
     // instance fields
     String productType;
 
-    public void advertise() {
-        System.out.println("Come spend some money!");
-        System.out.println("Selling " + productType + "!");
-    }
-
     // constructor method
     public Store(String product) {
         productType = product;
     }
 
     // advertise method
+    public void advertise() {
+        System.out.println("Selling " + productType + "!");
+        System.out.println("Come spend some money!");
+    }
 
     // main method
     public static void main(String[] args) {
-
+        Store lemonadeStand = new Store("Lemonade");
+        lemonadeStand.advertise();
+        lemonadeStand.advertise();
+        lemonadeStand.advertise();
     }
 }
