bdc03c20cae06a678a76c430269ea02e233c088a
Emiliano <emiliano.hotes@despegar.com>
2022-08-31 19:14:16 -0300

    feat: created Order calss - if statement

---
+++ 02-classes/Order.java
@@ -0,0 +1,12 @@
+public class Order {
+
+    public static void main(String[] args) {
+
+        double itemCost = 30.99;
+
+        // Write an if-then statement:
+        if(itemCost > 24)
+            System.out.print("High value item!");
+        }
+
+}
