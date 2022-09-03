8fe8dfe2eee98e53e4d5530e75850a697ffdd4d2
Emiliano <emiliano.hotes@despegar.com>
2022-09-03 10:33:45 -0300

    feat: add data to array

--- 04-arrays/ToDos.java
+++ 04-arrays/ToDos.java
@@ -1,12 +1,20 @@
-// import the ArrayList package here:
 import java.util.ArrayList;
 
 class ToDos {
 
     public static void main(String[] args) {
 
-        // Create toDoList below:
         ArrayList<String> toDoList = new ArrayList<String>();
+        String toDo1 = "Water plants";
+        // Add more to-dos here:
+        String toDo2 = "Go to supermarket";
+        String toDo3 = "Study Java";
+        // Add to-dos to toDoList
+        toDoList.add(toDo1);
+        toDoList.add(toDo2);
+        toDoList.add(toDo3);
+
+        System.out.println(toDoList);
 
     }
 
