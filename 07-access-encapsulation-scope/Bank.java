d68c467459863b96dde6fe9953a6f07b4dd4d1f0
Emiliano <emiliano.hotes@despegar.com>
2022-09-12 20:05:39 -0300

    feat: added accessors and mutators methods

--- 07-access-encapsulation-scope/Bank.java
+++ 07-access-encapsulation-scope/Bank.java
@@ -3,16 +3,15 @@ public class Bank{
     private CheckingAccount accountTwo;
 
     public Bank(){
-        accountOne = new CheckingAccount("Zeus", 100);
-        accountTwo = new CheckingAccount("Hades", 200);
+        accountOne = new CheckingAccount("Zeus", 100, "1");
+        accountTwo = new CheckingAccount("Hades", 200, "2");
     }
 
     public static void main(String[] args){
         Bank bankOfGods = new Bank();
-        System.out.println(bankOfGods.accountOne.name);
-        bankOfGods.accountOne.addFunds(5);
-        bankOfGods.accountOne.getInfo();
-
+        System.out.println(bankOfGods.accountOne.getBalance());
+        bankOfGods.accountOne.setBalance(5000);
+        System.out.println(bankOfGods.accountOne.getBalance());
     }
 
 }
--- 07-access-encapsulation-scope/CheckingAccount.java
+++ 07-access-encapsulation-scope/CheckingAccount.java
@@ -1,25 +1,20 @@
 public class CheckingAccount{
     private String name;
     private int balance;
+    private String id;
 
-    public CheckingAccount(String inputName, int inputBalance){
+    public CheckingAccount(String inputName, int inputBalance, String inputId){
         name = inputName;
         balance = inputBalance;
+        id = inputId;
     }
 
-    private void addFunds(int fundsToAdd){
-        balance += fundsToAdd;
+    //Write new methods here:
+    public int getBalance() {
+        return balance;
     }
 
-    private void getInfo(){
-        System.out.println("This checking account belongs to " + name +". It has " + balance + " dollars in it.");
+    public void setBalance(int newBalance) {
+        balance = newBalance;
     }
-
-    public static void main(String[] args){
-        CheckingAccount myAccount = new CheckingAccount("Persephone", 777);
-        System.out.println(myAccount.balance);
-        myAccount.addFunds(5);
-        System.out.println(myAccount.balance);
-    }
-
 }
