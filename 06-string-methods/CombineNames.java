28220d5ed4acfc7f9b5830fc8ce89d495c1a57f1
Emiliano <emiliano.hotes@despegar.com>
2022-09-12 19:05:35 -0300

    feat: added CombineNames class

---
+++ 06-string-methods/CombineNames.java
@@ -0,0 +1,14 @@
+public class CombineNames {
+
+    public static void main(String[] args) {
+
+        // Add your first names:
+        String firstName = "Emiliano ";
+        String lastName = "Hotes";
+
+        // What's the full name?
+        System.out.println(firstName.concat(lastName));
+
+    }
+
+}
