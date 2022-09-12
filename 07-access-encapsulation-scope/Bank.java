97360388320c32ca92db35beb9686c01032de94c
Emiliano <emiliano.hotes@despegar.com>
2022-09-12 19:50:06 -0300

    feat: added public keyword to variables and methods on CheckingAccount

--- 07-access-encapsulation-scope/Bank.java
+++ 07-access-encapsulation-scope/Bank.java
@@ -3,14 +3,16 @@ public class Bank{
     private CheckingAccount accountTwo;
 
     public Bank(){
-        accountOne = new CheckingAccount("Zeus", 100);
-        accountTwo = new CheckingAccount("Hades", 200);
+        accountOne = new CheckingAccount("Zeus", 100, "1");
+        accountTwo = new CheckingAccount("Hades", 200, "2");
     }
 
     public static void main(String[] args){
         Bank bankOfGods = new Bank();
         System.out.println(bankOfGods.accountOne.name);
-        System.out.println(bankOfGods.accountOne.balance);
+        bankOfGods.accountOne.addFunds(5);
+        bankOfGods.accountOne.getInfo();
+
     }
 
 }
